4f8939b0297cebfd2305d082863ab83dbbc489fe
boris-il-forte <boris.ilpossente@hotmail.it>
2017-11-17 17:43:07 +0100

    Added plot of state of the art comparison

---
+++ plots/grid_world_hasselt/all_algs.py
@@ -0,0 +1,38 @@
+import numpy as np
+from matplotlib import pyplot as plt
+#from matplotlib2tikz import save as tikz_save
+
+alg = ['Q', 'DQ', 'WQ', 'SPQ', 'RQ', 'RQ_Win']
+c = ['blue', 'red', 'cyan', 'orange', 'lawngreen', 'green']
+exp = ['1', '08']
+step = 20
+
+#base_folder = 'results/'
+base_folder = '/tmp/mushroom/grid_world_hasselt/'
+
+for e in exp:
+    plt.figure()
+    plt.title(e)
+    for i, a in enumerate(alg):
+        plt.subplot(2, 1, 1)
+        plt.plot(np.load(base_folder + a + '_' + e + '_r.npy')[::step], c[i], linewidth=3)
+        plt.subplot(2, 1, 2)
+        plt.plot(np.load(base_folder + a + '_' + e + '_maxQ.npy')[::step], c[i], linewidth=3)
+
+    plt.subplot(2, 1, 1)
+    plt.plot(np.ones(10000 / step) * 0.2, 'k', linewidth=1)
+    plt.subplot(2, 1, 2)
+    plt.xlabel('# steps')
+    plt.plot(np.ones(10000 / step) * 0.36, 'k', linewidth=1)    
+    
+    if e == '1':
+        plt.subplot(2, 1, 1)
+        plt.ylabel('Reward')
+        plt.subplot(2, 1, 2)
+        plt.ylabel('maxQ(s,a)')
+
+    if e == '08':
+        plt.subplot(2, 1, 1)
+        plt.legend(alg)
+plt.show()
+    #tikz_save('allAlgs' + e + '.tex', figureheight='5cm', figurewidth='6cm')
